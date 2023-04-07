401fc59874ae3c9b90093192879623bec8e35b47
LAPTOP-LI2S24TR\acer <rizkyfirda4693@gmail.com>
2023-04-07 14:32:59 +0700

    fck you

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Ini adalah aplikasi penghitung pH')
+st.header('Halo saya Rizky')
+st.subheader('Di sini saya akan mencoba melakukan penghitungan pH')
+st.text('Ini adalah teks')
+st.markdown("Pertama yaitu memasukkan Konsentrasi **:red[Titran]**")
+
